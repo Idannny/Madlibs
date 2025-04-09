fae02dd9ac8b2e8b6e3da745322b5df059890dfe
dannybwoi <dannyivan6@outlook.com>
2025-04-08 22:40:18 -0400

    configured config, display works

--- config.py
+++ config.py
@@ -5,74 +5,25 @@ class Config:
     # Common settings
     SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    STRIPE_KEY = os.getenv('STRIPE_SECRET')
+    STRIPE_PUBLISHABLE_KEY = os.getenv('STRIPE_PUBLISHABLE_KEY')
     
     # CSP Settings
     CSP = {
         'default-src': ["'self'"],
-        'script-src': [
-            "'self'",
-            'https://www.google.com/recaptcha/',
-            'https://www.gstatic.com/recaptcha/',
-            'https://js.stripe.com',
-            'https://cdn.jsdelivr.net'
-        ],
-        'style-src': [
-            "'self'",
-            'https://fonts.googleapis.com'
-        ],
-        'font-src': [
-            "'self'",
-            'https://fonts.gstatic.com'
-        ],
-        'img-src': [
-            "'self'",
-            'data:',
-            'https://lh3.googleusercontent.com',
-            'https://*.stripe.com',
-            'https://*.openai.com'
-        ],
-        'frame-src': [
-            'https://www.google.com/recaptcha/',
-            'https://js.stripe.com',
-            'https://hooks.stripe.com'
-        ],
-        'form-action': ["'self'"],
-        'frame-ancestors': ["'none'"],
-        'base-uri': ["'self'"],
-        'object-src': ["'none'"],
-        'upgrade-insecure-requests': True
+        'script-src': ["'self'", "'unsafe-inline'", "'unsafe-eval'"],
+        'img-src': ["'self'", 'data:', 'https:'],
+        'style-src': ["'self'", "'unsafe-inline'"],
     }
-
+      
 class DevelopmentConfig(Config):
     DEBUG = True
-    CSP = {
-        **Config.CSP,
-        'connect-src': [
-            "'self'",
-            'http://localhost:5000',
-            'https://www.google.com/recaptcha/',
-            'https://api.stripe.com',
-            'https://api.openai.com',
-            'https://*.stripe.com'
-        ]
-    }
 
 class ProductionConfig(Config):
     DEBUG = False
-    CSP = {
-        **Config.CSP,
-        'connect-src': [
-            "'self'",
-            'https://madapp.io',
-            'https://www.google.com/recaptcha/',
-            'https://api.stripe.com',
-            'https://api.openai.com',
-            'https://*.stripe.com'
-        ]
-    }
+
 
 config = {
     'development': DevelopmentConfig,
     'production': ProductionConfig,
-    'default': DevelopmentConfig
 } 
